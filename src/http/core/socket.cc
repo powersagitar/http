79b090efb67c930633022a98c2bad5d039d68f52
powersagitar <114868133+powersagitar@users.noreply.github.com>
2025-01-03 21:48:10 -0500

    Update copyright header

--- src/http/core/socket.cc
+++ src/http/core/socket.cc
@@ -1,6 +1,8 @@
+// Copyright 2025 powersagitar
 //
-// Created by powersagitar on 1/1/25.
-//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
 
 #include "socket.h"
 
